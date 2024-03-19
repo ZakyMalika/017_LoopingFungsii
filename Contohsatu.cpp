87486c69d08ab54b923c0ebd02f6e4d3bc8914da
ZakyMalika <zakymalika0987@gmail.com>
2024-03-19 19:53:35 +0700

    contoh 1

---
+++ Contohsatu.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    string nama[5];
+
+    for (i = 150; i > 5; i -= 10)
+    {
+        cout << i << ". "
+             << " SELAMAT PUASA" << endl;
+    }
+    cout << "Nilai i terakhir = " << i << endl;
+
+    for (i = 0; i < 5; i++)
+        ;
+    {
+        cout << "Masukan nama ke-" << i << ":";
+        cin >> nama[i];
+    }
+
+    for (i = 0; i < 5; i++)
+        ;
+    {
+        cout << "Nama ke-" << i << ":" << nama[i] << endl;
+    }
+}
